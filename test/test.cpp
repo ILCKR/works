18e13c9eeec7d1685a4f9e8e1637277153932081
ILCKR <rlylckr@gmail.com>
2022-02-11 18:46:32 +0300

    begin

---
+++ test/test.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <json/json.h>
+#include <boost/property_tree/ptree.hpp>
+#include <boost/property_tree/xml_parser.hpp>
+#include <boost/foreach.hpp>
+#include <string>
+#include <set>
+#include <exception>
+#include <iostream>
+
+namespace pt = boost::property_tree;
+
+using namespace std;
+
+
+void json() {
+	Json::Value root;
+	Json::StreamWriterBuilder builder;
+	const std::unique_ptr<Json::StreamWriter> writer(builder.newStreamWriter());
+	root["Name"] = "robin";
+	root["Age"] = 20;
+	writer->write(root, &std::cout);
+}
+
+int main()
+{
+
+	json();
+
+	system("PAUSE");
+	return 0;
+}
